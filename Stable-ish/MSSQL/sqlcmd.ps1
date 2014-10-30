dabfd15f5504243e08421e765c0ce31a2b10723f
Scott Sutherland <scott.sutherland@nullbind.com>
2014-10-30 16:08:53 -0500

    Update sqlcmd.ps1

--- Stable-ish/MSSQL/sqlcmd.ps1
+++ Stable-ish/MSSQL/sqlcmd.ps1
@@ -3,16 +3,13 @@
 	This script can be used to query sql servers and view the results.
 
 	.DESCRIPTION
-	This script can be used to query sql servers and view the results.
+	This script can be used to query sql servers and view the results with sql logins credentials, a trusted connection, 
+	or altenrative domain credential for both trusted and untrusted domains.
 
 	.EXAMPLE
 	Below is the basic usage.
 	PS C:\> Get-SqlServerSpSource -SQLServerInstance "SQLSERVER1\SQLEXPRESS" -query "select name from master..sysdatabases"
     
-    	.EXAMPLE
-	Below is the basic usage.
-	PS C:\> Get-SqlServerSpSource -SQLServerInstance "SQLSERVER1\SQLEXPRESS" -query "select name from master..sysdatabases"
-
 	.EXAMPLE
 	Run query against remote sql server that is on an untrusted domain with credentials from the same untrusted domain.  I'll
 	update the script to switches to automatically generate the credential manager entries later.  This can also be used to connect
