99d4db14e71159f54d3cbd32ad2c24956360ad49
Scott Sutherland <scott.sutherland@nullbind.com>
2014-10-30 22:41:08 -0500

    Update Invoke-SqlCmd.ps1

--- Stable-ish/MSSQL/Invoke-SqlCmd.ps1
+++ Stable-ish/MSSQL/Invoke-SqlCmd.ps1
@@ -4,7 +4,8 @@
 
 	.DESCRIPTION
 	This script can be used to query SQL Servers and view the results without needing the binaries that ship with SQL Server.
-	It can be used with trusted connections, SQL Server logins, and alter domain credentials.  Authentication is supported for both trusted and untrusted domains.
+    It can be used with trusted connections, SQL Server logins, and alter domain credentials.  Authentication is supported
+    for both trusted and untrusted domains.
 
 	.EXAMPLE
 	Below is an example of how to query a SQL Server using the current Windows user context or "trusted connection".
@@ -36,80 +37,86 @@
     [string]$SqlServerInstance
 )
 
+Begin
+{
+    # -----------------------------------------------
+    # Connect to the sql server
+    # -----------------------------------------------
+    # Create fun connection object
+    $conn = New-Object System.Data.SqlClient.SqlConnection
 
-# -----------------------------------------------
-# Connect to the sql server
-# -----------------------------------------------
-# Create fun connection object
-$conn = New-Object System.Data.SqlClient.SqlConnection
-
-# Check for domain credentials
-if($SqlUser){
-    $DomainUserCheck = $SqlUser.Contains("\")
-}
-
-# Set authentication type and create connection string
-if($SqlUser -and $SqlPassword -and !$DomainUserCheck){
-    
-    # SQL login / alternative domain credentials
-    $conn.ConnectionString = "Server=$SqlServerInstance;Database=master;User ID=$SqlUser;Password=$SqlPass;"
-    [string]$ConnectUser = $SqlUser
-}else{
-
-    # Create credentials management entry if a domain user is used
-    if ($DomainUserCheck -and (Test-Path  ("C:\Windows\System32\cmdkey.exe"))){   
-     		
-        Write-Output "[*] Attempting to authenticate to $SqlServerInstance with domain account $SqlUser..."
-        $SqlServerInstanceCol = $SqlServerInstance -replace ',', ':'
-	    $CredManCmd = 'cmdkey /add:'+$SqlServerInstanceCol+' /user:'+$SqlUser+' /pass:'+$SqlPass 
-        Write-Verbose "Command: $CredManCmd"
-        $ExecManCmd = invoke-expression $CredManCmd
-    }else{
-
-        Write-Output "[*] Attempting to authenticate to $SqlServerInstance as the current Windows user..."
+    # Check for domain credentials
+    if($SqlUser){
+        $DomainUserCheck = $SqlUser.Contains("\")
     }
 
-    # Trusted connection
-    $conn.ConnectionString = "Server=$SqlServerInstance;Database=master;Integrated Security=SSPI;"   
-    $UserDomain = [Environment]::UserDomainName
-    $Username = [Environment]::UserName
-    $ConnectUser = "$UserDomain\$Username"
+    # Set authentication type and create connection string
+    if($SqlUser -and $SqlPassword -and !$DomainUserCheck){
+    
+        # SQL login / alternative domain credentials
+        $conn.ConnectionString = "Server=$SqlServerInstance;Database=master;User ID=$SqlUser;Password=$SqlPass;"
+        [string]$ConnectUser = $SqlUser
+    }else{
+
+        # Create credentials management entry if a domain user is used
+        if ($DomainUserCheck -and (Test-Path  ("C:\Windows\System32\cmdkey.exe"))){   
+     		
+            Write-Output "[*] Attempting to authenticate to $SqlServerInstance with domain account $SqlUser..."
+            $SqlServerInstanceCol = $SqlServerInstance -replace ',', ':'
+	        $CredManCmd = 'cmdkey /add:'+$SqlServerInstanceCol+' /user:'+$SqlUser+' /pass:'+$SqlPass 
+            Write-Verbose "Command: $CredManCmd"
+            $ExecManCmd = invoke-expression $CredManCmd
+        }else{
+
+            Write-Output "[*] Attempting to authenticate to $SqlServerInstance as the current Windows user..."
+        }
+
+        # Trusted connection
+        $conn.ConnectionString = "Server=$SqlServerInstance;Database=master;Integrated Security=SSPI;"   
+        $UserDomain = [Environment]::UserDomainName
+        $Username = [Environment]::UserName
+        $ConnectUser = "$UserDomain\$Username"
+    }
+
+    # Attempt database connection
+    try{
+        $conn.Open()
+        write-host "[*] Connected." -foreground "green"
+    }catch{
+        $ErrorMessage = $_.Exception.Message
+        write-host "[*] Connection failed" -foreground "red"
+        write-host "[*] Error: $ErrorMessage" -foreground "red"
+        Break
+    }
+}
+Process
+{
+
+    # -----------------------------------------------
+    # Send query to server and process results
+    # -----------------------------------------------
+    $cmd = New-Object System.Data.SqlClient.SqlCommand($query,$conn)
+    $results = $cmd.ExecuteReader()
+    $MyQueryResults = New-Object System.Data.DataTable
+    $MyQueryResults.Load($results)
+    $MyQueryResults
+
+    # Disconnect from database
+    $conn.Close()
 }
 
-# Attempt database connection
-try{
-    $conn.Open()
-    write-host "[*] Connected." -foreground "green"
-}catch{
-    $ErrorMessage = $_.Exception.Message
-    write-host "[*] Connection failed" -foreground "red"
-    write-host "[*] Error: $ErrorMessage" -foreground "red"
+End
+{
+    # -----------------------------------------------
+    # Clean up 
+    # -----------------------------------------------
 
-    # -----------------------------------------------
-    # Clean up credentials manager entry
-    # -----------------------------------------------
-     if ($DomainUserCheck){
-        $CredManDel = 'cmdkey /delete:'+$SqlServerInstanceCol
-        Write-Verbose "Command: $CredManDel"   
+    # Remove credentials manager entry
+    if ($DomainUserCheck){
+        $CredManDel = 'cmdkey /delete:'+$SqlServerInstanceCol   
         $ExecManDel = invoke-expression $CredManDel
-     }
-    Break
-}
+    }
 
-# -----------------------------------------------
-# Send query to server and process results
-# -----------------------------------------------
-$cmd = New-Object System.Data.SqlClient.SqlCommand($query,$conn)
-$results = $cmd.ExecuteReader()
-$MyQueryResults = New-Object System.Data.DataTable
-$MyQueryResults.Load($results)
-$MyQueryResults
-
-# -----------------------------------------------
-# Clean up credentials manager entry
-# -----------------------------------------------
- if ($DomainUserCheck){
-    $CredManDel = 'cmdkey /delete:'+$SqlServerInstanceCol
-    Write-Verbose "Command: $CredManDel"   
-    $ExecManDel = invoke-expression $CredManDel
+    # Status user
+    Write-Output "[*] Done." 
  }
