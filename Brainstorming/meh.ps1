04f4e3868530c9b028eaee11f6eaadc82e106cf7
Scott Sutherland <scott.sutherland@nullbind.com>
2015-06-15 14:38:32 -0500

    Update meh.ps1

--- Brainstorming/meh.ps1
+++ Brainstorming/meh.ps1
@@ -3,24 +3,35 @@
 # http://blogs.technet.com/b/ashleymcglone/archive/2013/03/25/active-directory-ou-permissions-report-free-powershell-script-download.aspx
 
 # Create data table for inventory of object typees
-$TableDacl = New-Object System.Data.DataTable 
-$TableDacl.Columns.Add('GuidNumber')| Out-Null
-$TableDacl.Columns.Add('GuidName')| Out-Null
-$TableDacl.Clear| Out-Null
+$TableTypes = New-Object System.Data.DataTable 
+$TableTypes.Columns.Add('GuidNumber')| Out-Null
+$TableTypes.Columns.Add('GuidName')| Out-Null
+$TableTypes.Clear| Out-Null
 
 # Get standard rights
-Write-Host "Indexing standard rights"
+Write-Host "Getting standard rights"
 Get-ADObject -SearchBase (Get-ADRootDSE).schemaNamingContext -LDAPFilter '(schemaIDGUID=*)' -Properties name, schemaIDGUID |
 ForEach-Object {
-    $TableDacl.Rows.Add([System.GUID]$_.schemaIDGUID,$_.name) | Out-Null
+    $TableTypes.Rows.Add([System.GUID]$_.schemaIDGUID,$_.name) | Out-Null
 }
 
 # Get extended rights
-Write-Host "Indexing extended rights"
+Write-Host "Getting extended rights"
 Get-ADObject -SearchBase "CN=Extended-Rights,$((Get-ADRootDSE).configurationNamingContext)" -LDAPFilter '(objectClass=controlAccessRight)' -Properties name, rightsGUID |
 ForEach-Object {
-   # $TableDacl.Rows.Add([System.GUID]$_.rightsGUID,$_.name)
+   $TableTypes.Rows.Add([System.GUID]$_.rightsGUID,$_.name) | Out-Null
 }
 
-Write-Host "Display results"
-$TableDacl | select guidnumber,guidname -Unique | Sort guidname
+# Select guidname based on guidnumber
+$ObjectType = "bf967a7f-0de6-11d0-a285-00aa003049e2"
+$ObjectTypeGuid = "'" + "$ObjectType" + "'"
+$ObjectTypeGuidCount = $TableTypes.Select("guidnumber = $ObjectTypeGuid").Count
+if ($ObjectTypeGuidCount -gt 0){
+    [string]$ObjectTypeName = $TableTypes.Select("guidnumber=$ObjectTypeGuid") | select guidname -ExpandProperty guidname -First 1   
+}else{
+    [string]$ObjectTypeName = ""
+}
+
+$ObjectTypeName
+
+
