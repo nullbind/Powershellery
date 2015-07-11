4218977979ca934a9e515ce882423cbbd807821b
Scott Sutherland <scott.sutherland@nullbind.com>
2015-07-11 10:12:28 -0500

    Update Invoke-SqlServerServiceImpersonation-Cmd.ps1

--- Stable-ish/MSSQL/Invoke-SqlServerServiceImpersonation-Cmd.ps1
+++ Stable-ish/MSSQL/Invoke-SqlServerServiceImpersonation-Cmd.ps1
@@ -1,3 +1,11 @@
+# script: Invoke-SqlServerServiceImpersonation-Cmd.ps1
+# author: scott sutherland (@_nullbind), 2015 netspi
+# Description: This script enumerates running sql server processes and 
+# opens a cmd.exe console as each of the service account.  This can be
+# used as to gain access to the sql server if the sa password is lost or locked.
+#...also if a fun demo during pentests.
+# credits: JosephBialek for invoke-mikatz.ps1 and benjamin delpy for the original mimikatz.
+
 Write-Host "Getting list of SQL Server services..."
 $SqlServices = Get-WmiObject -Class win32_service | where {$_.pathname -like "*Microsoft SQL Server*"} | select displayname,pathname,StartName 
 $RunningProc = Get-WmiObject -Class win32_process | select processid,ExecutablePath
