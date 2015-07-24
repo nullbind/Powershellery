9f7c58e73ca14b1f295c93efeac6139556a76fca
Scott Sutherland <scott.sutherland@nullbind.com>
2015-07-24 10:33:55 -0500

    Update Get-LinkCrawlStats.ps1

--- Stable-ish/MSSQL/Get-LinkCrawlStats.ps1
+++ Stable-ish/MSSQL/Get-LinkCrawlStats.ps1
@@ -1,6 +1,6 @@
 
 # .\Get-LinkCrawlStats.ps1 -CrawlFile .\msfoutput.csv
-# this script can be used to parse output from the exploit/windows/mssql/mssql_linkcrawler metasploit module.
+# this script can be used to prase output from the sql server link crawler module.
 [CmdletBinding()]
 	Param(
 	
@@ -17,9 +17,9 @@
     $LinkCount_Up = $file | Where-Object {$_.link_state -like "up"} | select link_server | measure | select Count -ExpandProperty Count
     $LinkCount_Sysadmin = $file | Where-Object {$_.link_privilege -like "SYSADMIN!"} | select link_server | measure | select Count -ExpandProperty Count
     $LinkCount_User = $file | Where-Object {$_.link_privilege -notlike "SYSADMIN!" -and $_.link_state -like "up"} | select link_server | measure | select Count -ExpandProperty Count
-    $ServerCount_Total = $file | select db_server -Unique | measure | select Count -ExpandProperty Count
-    $ServerCount_Sysadmin = $file | Where-Object {$_.link_privilege -like "SYSADMIN!"} | select db_server -Unique | measure | select Count -ExpandProperty Count
-    $ServerCount_User = $file | Where-Object {$_.link_privilege -notlike "SYSADMIN!" -and $_.link_state -like "up"} | select link_server -Unique | measure | select Count -ExpandProperty Count
+    $ServerCount_Total = $file | select db_server | Sort-Object -Unique db_server|  measure | select Count -ExpandProperty Count
+    $ServerCount_Sysadmin = $file | Where-Object {$_.link_privilege -like "SYSADMIN!"} | select db_server | Sort-Object -Unique db_server | measure | select Count -ExpandProperty Count
+    $ServerCount_User = $file | Where-Object {$_.link_privilege -notlike "SYSADMIN!" -and $_.link_state -like "up"} | Sort-Object -Unique link_server | measure | select Count -ExpandProperty Count
 
     # Display counts
     Write-Output " "
@@ -31,10 +31,11 @@
     Write-Output "Total Live Servers with User privs:$ServerCount_User"
     Write-Output "Total Live Servers with Sysadmin privs:$ServerCount_Sysadmin"
 
-    $LinkUser = $file |  select link_user -Unique | Where-Object {$_.link_user -notlike ""} 
-    $LinkUserCount = $LinkUser.count    
+    $Linkusers = $file |  select link_user | Where-Object {$_.link_user -notlike ""} | Sort-Object -Unique link_user   
+    $LinkUserCount = $LinkUsers.count    
     Write-Output "Total Link Users: $LinkUserCount "
-    $LinkUser
+    $LinkUsers
     
+
 
 
