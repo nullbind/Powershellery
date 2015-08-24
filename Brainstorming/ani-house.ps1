4b97bd35b149866dda569d0e9c78ad9213210690
Scott Sutherland <scott.sutherland@nullbind.com>
2015-08-24 09:01:05 -0500

    Create ani-house.ps1

---
+++ Brainstorming/ani-house.ps1
@@ -0,0 +1,93 @@
+function four
+{
+    clear
+    Write-Output "   Weeeeeee!                        "
+    Write-Output "                          (  )"    
+    Write-Output "                ________ (  )"
+    Write-Output "               //\------\( )"
+    Write-Output "              //  \------ |"
+    Write-Output "             //    \------\"
+    Write-Output "            //      \------\"
+    Write-Output "           //========\______\     (())"
+    Write-Output "            |    ___ |=-   |     ((()))"
+    Write-Output "            |   |   ||=-   |    (((())))"
+    Write-Output "            |   |  o||=-   |      |__|"
+    Write-Output "    ...(````)  ___|___||_____|//.,  | o|,," 
+    Write-Output " ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+    write-output "  "
+    Write-Output "              Loading exploit...             "
+    sleep -Seconds 1
+}
+
+function three
+{
+    clear
+    Write-Output "   Weeeee                        "
+    Write-Output "                          "    
+    Write-Output "                ________ (  )"
+    Write-Output "               //\------\( )"
+    Write-Output "              //  \------ |"
+    Write-Output "             //    \------\"
+    Write-Output "            //      \------\"
+    Write-Output "           //========\______\     (())"
+    Write-Output "            |    ___ |=-   |     ((()))"
+    Write-Output "            |   |   ||=-   |    (((())))"
+    Write-Output "            |   |  o||=-   |      |__|"
+    Write-Output "    ...(````)  ___|___||_____|//.,  | o|,," 
+    Write-Output " ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+    write-output "  "
+    Write-Output "              Loading exploit...             "
+    sleep -Seconds 1
+}
+
+function two
+{
+    clear
+    Write-Output "   Weee                        "
+    Write-Output "                          "    
+    Write-Output "                ________  "
+    Write-Output "               //\------\( )"
+    Write-Output "              //  \------ |"
+    Write-Output "             //    \------\"
+    Write-Output "            //      \------\"
+    Write-Output "           //========\______\     (())"
+    Write-Output "            |    ___ |=-   |     ((()))"
+    Write-Output "            |   |   ||=-   |    (((())))"
+    Write-Output "            |   |  o||=-   |      |__|"
+    Write-Output "    ...(````)  ___|___||_____|//.,  | o|,,"  
+    Write-Output " ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+    write-output "  "
+    Write-Output "              Loading exploit...             "
+    sleep -Seconds 1
+}
+
+function one
+{
+    clear
+    Write-Output "   We                        "
+    Write-Output "                          "    
+    Write-Output "                ________  "
+    Write-Output "               //\------\"
+    Write-Output "              //  \------ |"
+    Write-Output "             //    \------\"
+    Write-Output "            //      \------\"
+    Write-Output "           //========\______\     (())"
+    Write-Output "            |    ___ |=-   |     ((()))"
+    Write-Output "            |   |   ||=-   |    (((())))"
+    Write-Output "            |   |  o||=-   |      |__|"
+    Write-Output "    ...(````)  ___|___||_____|//.,  | o|,,"  
+    Write-Output " ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+    write-output "  "
+    Write-Output "              Loading exploit...             "
+
+    sleep -Seconds 1
+}
+
+
+# Run animation
+1..100|%{
+one 
+two 
+three
+four
+}
