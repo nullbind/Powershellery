be1b04f6dcfe09efb2b26f56e2c3a8a8d2f55d40
Scott Sutherland <scott.sutherland@nullbind.com>
2015-08-24 09:00:46 -0500

    Create anti-circle.ps1

---
+++ Brainstorming/anti-circle.ps1
@@ -0,0 +1,231 @@
+function one { 
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     ...... . .     ."
+	write-output "   .........          ."
+	write-output "  .........            ."
+	write-output " ........               ."
+	write-output " .......                .  "
+	write-output " ......                 .  "
+	write-output " .....                  . "
+	write-output "  ....                  ."
+	write-output "   . .                 ."
+	write-output "     .               ."
+	write-output "       . . . . .  ."
+}
+
+
+function two { 
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .   .. . .     ."
+	write-output "   .   .....          ."
+	write-output "  .   .....            ."
+	write-output " .   ....               ."
+	write-output " .  ....                .  "
+	write-output " .  ...                 .  "
+	write-output " .  ..                  . "
+	write-output "  . ..                  ."
+	write-output "   . .                 ."
+	write-output "     .               ."
+	write-output "       . . . . .  ."
+}
+
+
+function three {
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .    .......   ."
+	write-output "   .    ......        ."
+	write-output "  .    ....            ."
+	write-output " .    ...               ."
+	write-output " .   ...                .  "
+	write-output " .   ..                 .  "
+	write-output " .   ..                 . "
+	write-output "  .  ..                 ."
+	write-output "   . ..                ."
+	write-output "     . .              ."
+	write-output "       . . . . .  ."
+}
+
+function four{
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .     .......  ."
+	write-output "   .     .......      ."
+	write-output "  .     .....          ."
+	write-output " .      ....            ."
+	write-output " .      ...             .  "
+	write-output " .      ...             .  "
+	write-output " .      ..              . "
+	write-output "  .     ..             ."
+	write-output "   .     ..           ."
+	write-output "     .    ..         ."
+	write-output "       . . . . .  ."
+}
+
+
+function five {
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .          ..   ."
+	write-output "   .          ..       ."
+	write-output "  .        .....        ."
+	write-output " .        .....          ."
+	write-output " .        .....          .  "
+	write-output " .        ....           .  "
+	write-output " .         ....          . "
+	write-output "  .         ....        ."
+	write-output "   .         ...       ."
+	write-output "     .        ...    ."
+	write-output "       . . . . .  ."
+}
+
+
+
+function six {
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .               ."
+	write-output "   .-                  ."
+	write-output "  . o         ..... .   ."
+	write-output " .          ...        . ."
+	write-output " .          ...          .  "
+	write-output " .          ..           .  "
+	write-output " .           ..          . "
+	write-output "  .          ...        ."
+	write-output "   .          ....   ..."
+	write-output "     .          ......"
+	write-output "       . . . . .  ."
+}
+
+
+function seven {
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .               ."
+	write-output "   .-       -          ."
+	write-output "  . o      o            ."
+	write-output " .                       ."
+	write-output " .                       .  "
+	write-output " .                 ..... .  "
+	write-output " .                ..   ... "
+	write-output "  .                ..  .."
+	write-output "   .                ...."
+	write-output "     .             ..."
+	write-output "       . . . . .  ."
+}
+
+
+
+function eight {
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .               ."
+	write-output "   .                   ."
+	write-output "  .      _      _       ."
+	write-output " .       o      o        ."
+	write-output " .                       .  "
+	write-output " .                    .. .  "
+	write-output " .                     ... "
+	write-output "  .                    .."
+	write-output "   .                ...."
+	write-output "     .             ..."
+	write-output "       . . . . .  ."
+}
+
+
+function nine {
+	sleep -Seconds 1
+	clear
+	write-output "        . . . . . .  "
+	write-output "     .               ."
+	write-output "   .                   ."
+	write-output "  .      ___   ___      ."
+	write-output " .       o      o        ."
+	write-output " .                       .  "
+	write-output " .                       .  "
+	write-output " .                       . "
+	write-output "  .                     ."
+	write-output "   .        _          ."
+	write-output "     .               ."
+	write-output "       . . . . .  ."
+	write-output " "
+}
+
+
+function ten {
+	sleep -Seconds 1
+	clear
+	write-output "         . . . . .  "
+	write-output "     .               ."
+	write-output "   .     ___   ___     ."
+	write-output "  .                     ."
+	write-output " .       o      o        ."
+	write-output " .                       .  "
+	write-output " .                       .  "
+	write-output " .                       . "
+	write-output "  .                     ."
+	write-output "   .        ___        ."
+	write-output "     .               ."
+	write-output "       . . . . .  ."
+}
+
+
+function eleven {
+	sleep -Seconds 1
+	clear
+	write-output "         . . . . .  "
+	write-output "     .               ."
+	write-output "   .     ___   ___     ."
+	write-output "  .                     ."
+	write-output " .       o      o        ."
+	write-output " .                       .  "
+	write-output " .                       .  "
+	write-output " .                       . "
+	write-output "  .        _____        ."
+	write-output "   .       \___/       ."
+	write-output "     .               ."
+	write-output "       . . . . .  ."
+}
+
+function twelve {
+	sleep -Seconds 1
+	clear
+	write-output "         . . . . .  "
+	write-output "     .               ."
+	write-output "   .     /       \     ."
+	write-output "  .                     ."
+	write-output " .       o      o        ."
+	write-output " .                       .  "
+	write-output " .                       .  "
+	write-output " .        _______        . "
+	write-output "  .       \     /       ."
+	write-output "   .       \___/       ."
+	write-output "     .               ."
+	write-output "   	  . . . . .  .  Weeee!"
+}
+
+1..100 | %{
+    one
+    two
+    three
+    four
+    five
+    six
+    seven
+    eight
+    nine
+    ten 
+    eleven
+    twelve
+
+}
