4ee871d7ad8877aa72a91dc81fd6128c107db9e5
Scott Sutherland <scott.sutherland@nullbind.com>
2016-03-06 22:19:52 -0600

    Update Get-CachedPlans.sql

--- Stable-ish/MSSQL/templates-tsql/Get-CachedPlans.sql
+++ Stable-ish/MSSQL/templates-tsql/Get-CachedPlans.sql
@@ -3,7 +3,8 @@
 -- Description: Returns a row for each query plan that is cached by SQL Server for faster query execution. 
 -- Reference: https://msdn.microsoft.com/en-us/library/ms187404.aspx
 
-SELECT bucketid,plan_handle,cacheobjtype,objtype,dbid,DB_NAME(dbid) as DatabaseName,objectid,OBJECT_NAME(objectid) as ObjectName,refcounts,usecounts,number,encrypted,text
+SELECT  bucketid,plan_handle,size_in_bytes,cacheobjtype,objtype,dbid,DB_NAME(dbid) as DatabaseName,objectid,OBJECT_NAME(objectid) as ObjectName,refcounts,usecounts,number,encrypted,text
 FROM sys.dm_exec_cached_plans AS p
-CROSS APPLY sys.dm_exec_sql_text(p.plan_handle) AS t 
+CROSS APPLY sys.dm_exec_sql_text(p.plan_handle) AS t
 ORDER BY usecounts DESC
+
