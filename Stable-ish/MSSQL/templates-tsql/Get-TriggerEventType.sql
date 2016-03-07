0fcfb0ee06d376887b2cc76f078219cdc159d00e
Scott Sutherland <scott.sutherland@nullbind.com>
2016-03-07 15:10:27 -0600

    Update Get-TriggerEventType.sql

--- Stable-ish/MSSQL/templates-tsql/Get-TriggerEventType.sql
+++ Stable-ish/MSSQL/templates-tsql/Get-TriggerEventType.sql
@@ -1,7 +1,7 @@
 -- Script: Get-TriggerEventType.sql
 -- Requirements: Sysadmin or required SELECT privileges.
 -- Description: Returns trigger event types. 
--- Reference: https://msdn.microsoft.com/en-us/library/bb510453.aspx
+-- Reference: https://msdn.microsoft.com/en-us/library/bb522542.aspx
 
 SELECT *
 FROM sys.trigger_event_types
