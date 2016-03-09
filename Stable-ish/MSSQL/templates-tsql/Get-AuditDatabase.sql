c36a219fb815289610c6050ccbd6b543d433c970
Scott Sutherland <scott.sutherland@nullbind.com>
2016-03-08 23:01:06 -0600

    Update Get-AuditDatabase.sql

--- Stable-ish/MSSQL/templates-tsql/Get-AuditDatabase.sql
+++ Stable-ish/MSSQL/templates-tsql/Get-AuditDatabase.sql
@@ -3,14 +3,14 @@
 -- Reference: https://technet.microsoft.com/en-us/library/ms190227(v=sql.110).aspx
 
 SELECT	a.audit_id,
-		a.name as audit_name,
-		s.name as database_specification_name,
-		d.audit_action_name,
-		s.is_state_enabled,
-		d.is_group,
-		s.create_date,
-		s.modify_date,
-		d.audited_result
+	a.name as audit_name,
+	s.name as database_specification_name,
+	d.audit_action_name,
+	s.is_state_enabled,
+	d.is_group,
+	s.create_date,
+	s.modify_date,
+	d.audited_result
 FROM sys.server_audits AS a
 JOIN sys.database_audit_specifications AS s
 ON a.audit_guid = s.audit_guid
