98345e731ce81f180fc44ecee4547e34e64f7d00
Scott Sutherland <scott.sutherland@nullbind.com>
2016-04-28 22:13:49 -0500

    Update Get-ServiceAccount.sql

--- Stable-ish/MSSQL/templates-tsql/Get-ServiceAccount.sql
+++ Stable-ish/MSSQL/templates-tsql/Get-ServiceAccount.sql
@@ -3,50 +3,67 @@
 -- Reference: http://stackoverflow.com/questions/4440141/differences-between-xp-instance-regread-and-xp-regread
 -- TODO: Add privilege checks.
 
-DECLARE       @DBEngineLogin		VARCHAR(100)
-DECLARE       @AgentLogin			VARCHAR(100)
-DECLARE       @BrowserLogin			VARCHAR(100)
-DECLARE       @WriterLogin          VARCHAR(100)
-DECLARE		  @AnalysisLogin		VARCHAR(100)
-DECLARE		  @ReportLogin			VARCHAR(100)
-DECLARE		  @IntegrationLogin		VARCHAR(100)
-	
- EXECUTE       master.dbo.xp_instance_regread
-              @rootkey      = N'HKEY_LOCAL_MACHINE',
-              @key          = N'SYSTEM\CurrentControlSet\Services\MSSQLServer',
-              @value_name   = N'ObjectName',
-              @value        = @DBEngineLogin OUTPUT
+-- Setup variables
+DECLARE		@SQLServerInstance	VARCHAR(250)  
+DECLARE		@MSOLAPInstance		VARCHAR(250) 
+DECLARE		@ReportInstance 	VARCHAR(250) 
+DECLARE		@AgentInstance	 	VARCHAR(250) 
+DECLARE		@DBEngineLogin		VARCHAR(100)
+DECLARE		@AgentLogin		VARCHAR(100)
+DECLARE		@BrowserLogin		VARCHAR(100)
+DECLARE     	@WriterLogin		VARCHAR(100)
+DECLARE		@AnalysisLogin		VARCHAR(100)
+DECLARE		@ReportLogin		VARCHAR(100)
+DECLARE		@IntegrationLogin	VARCHAR(100)
 
-EXECUTE       master.dbo.xp_instance_regread
-              @rootkey      = N'HKEY_LOCAL_MACHINE',
-              @key          = N'SYSTEM\CurrentControlSet\Services\SQLServerAgent',
-              @value_name   = N'ObjectName',
-              @value        = @AgentLogin OUTPUT
+-- Get SQL Server Service Name Path 
+if @@SERVICENAME = 'MSSQLSERVER'
+	BEGIN											
+	set @SQLServerInstance = 'SYSTEM\CurrentControlSet\Services\MSSQLSERVER'
+	END						
+ELSE
+	BEGIN
+	set @SQLServerInstance = 'SYSTEM\CurrentControlSet\Services\MSSQL$' + cast(@@SERVICENAME as varchar(250))	
+	set @MSOLAPInstance = 'SYSTEM\CurrentControlSet\Services\MSOLAP$' + cast(@@SERVICENAME as varchar(250))		
+	set @ReportInstance = 'SYSTEM\CurrentControlSet\Services\ReportServer$' + cast(@@SERVICENAME as varchar(250))
+	set @AgentInstance = 'SYSTEM\CurrentControlSet\Services\SQLAgent$' + cast(@@SERVICENAME as varchar(250))							
+	END
 
+-- Get SQL Server - Calculated
+EXECUTE		master.dbo.xp_instance_regread  
+			N'HKEY_LOCAL_MACHINE', @SQLServerInstance,  
+			N'ObjectName',@DBEngineLogin OUTPUT
+
+-- Get SQL Server Agent - Calculated
+EXECUTE		master.dbo.xp_instance_regread  
+			N'HKEY_LOCAL_MACHINE', @AgentInstance,  
+			N'ObjectName',@AgentLogin OUTPUT
+
+-- Get SQL Server Browser - Static Location
 EXECUTE       master.dbo.xp_instance_regread
               @rootkey      = N'HKEY_LOCAL_MACHINE',
               @key          = N'SYSTEM\CurrentControlSet\Services\SQLBrowser',
               @value_name   = N'ObjectName',
               @value        = @BrowserLogin OUTPUT
 
+-- Get SQL Server Writer - Static Location
 EXECUTE       master.dbo.xp_instance_regread
               @rootkey      = N'HKEY_LOCAL_MACHINE',
               @key          = N'SYSTEM\CurrentControlSet\Services\SQLWriter',
               @value_name   = N'ObjectName',
               @value        = @WriterLogin OUTPUT
 
-EXECUTE       master.dbo.xp_instance_regread
-              @rootkey      = N'HKEY_LOCAL_MACHINE',
-              @key          = N'SYSTEM\CurrentControlSet\Services\MSOLAP$STANDARD',
-              @value_name   = N'ObjectName',
-              @value        = @AnalysisLogin OUTPUT
+-- Get MSOLAP - Calculated
+EXECUTE		master.dbo.xp_instance_regread  
+			N'HKEY_LOCAL_MACHINE', @MSOLAPInstance,  
+			N'ObjectName',@AnalysisLogin OUTPUT
 
-EXECUTE       master.dbo.xp_instance_regread
-              @rootkey      = N'HKEY_LOCAL_MACHINE',
-              @key          = N'SYSTEM\CurrentControlSet\Services\ReportServer$STANDARD',
-              @value_name   = N'ObjectName',
-              @value        = @ReportLogin OUTPUT
+-- Get Reporting - Calculated
+EXECUTE		master.dbo.xp_instance_regread  
+			N'HKEY_LOCAL_MACHINE', @ReportInstance,  
+			N'ObjectName',@ReportLogin OUTPUT
 
+-- Get SQL Server DTS Server
 EXECUTE       master.dbo.xp_instance_regread
               @rootkey      = N'HKEY_LOCAL_MACHINE',
               @key          = N'SYSTEM\CurrentControlSet\Services\MsDtsServer110',
@@ -54,11 +71,10 @@ EXECUTE       master.dbo.xp_instance_regread
               @value        = @IntegrationLogin OUTPUT
 
 SELECT	[DBEngineLogin] = @DBEngineLogin, 
-	[AgentLogin] = @AgentLogin,
-	[BrowserLogin] = @BrowserLogin,
-	[WriterLogin] = @WriterLogin,
-	[AnalysisLogin] = @AnalysisLogin,
-	[ReportLogin] = @ReportLogin,
-	[IntegrationLogin] = @IntegrationLogin
-
+		[BrowserLogin] = @BrowserLogin,
+		[AgentLogin] = @AgentLogin,
+		[WriterLogin] = @WriterLogin,
+		[AnalysisLogin] = @AnalysisLogin,
+		[ReportLogin] = @ReportLogin,
+		[IntegrationLogin] = @IntegrationLogin
 GO
