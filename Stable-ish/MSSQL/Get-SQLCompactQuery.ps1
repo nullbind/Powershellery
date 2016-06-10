2367ff050bb1c5916b398c5651cd2b36c6755bae
Scott Sutherland <nullbind@users.noreply.github.com>
2016-06-10 11:56:17 -0500

    Update Get-SQLCompactQuery.ps1

--- Stable-ish/MSSQL/Get-SQLCompactQuery.ps1
+++ Stable-ish/MSSQL/Get-SQLCompactQuery.ps1
@@ -1,16 +1,44 @@
 # This script can be used as a template for querying sql server compact edition files
+# Example: .\Get-SQLCompactQuery.ps1 -Query "select 2" -DbFilePath c:\temp\file.sdf -Password SecretPassword!
+
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$false)]
+   [string]$LibFilePath,
+	
+   [Parameter(Mandatory=$true)]
+   [string]$DbFilePath,
+
+   [Parameter(Mandatory=$false)]
+   [string]$Password,
+
+   [Parameter(Mandatory=$false)]
+   [string]$Query = "Select 1"
+)
+
+# Define stuff
+if (-not $libpath){
+    $libpath = "C:\Program Files (x86)\Microsoft SQL Server Compact Edition\v4.0\Desktop\System.Data.SqlServerCe.dll"
+}
 
 # Import required library
-[Reflection.Assembly]::LoadFile("C:\Program Files (x86)\Microsoft SQL Server Compact Edition\v4.0\Desktop\System.Data.SqlServerCe.dll")
+[Reflection.Assembly]::LoadFile("$libpath") | Out-Null
+
+# Setup up password if provided
+if($Password){
+    $DbPass = ";Password=`"$Password`""
+}else{
+    $DbPass = ""
+}
 
 # Setup connection string
-$connString = "Data Source='C:\temp\file.sdf';Password='password'" 
+$connString = "Data Source=`"$DbFilePath`"$DbPass" 
 $cn = new-object "System.Data.SqlServerCe.SqlCeConnection" $connString
 
 # Create the command 
 $cmd = new-object "System.Data.SqlServerCe.SqlCeCommand"
 $cmd.CommandType = [System.Data.CommandType]"Text" 
-$cmd.CommandText = "select 1" 
+$cmd.CommandText = "$Query" 
 $cmd.Connection = $cn
 
 # Create data table to store results
