573ff3bf228a1bf3b7f29a2ab9d3a8122387443a
Scott Sutherland <nullbind@users.noreply.github.com>
2017-08-29 12:56:57 -0500

    Update Get-SQLOSFileOpenRowSet.sql

--- Stable-ish/MSSQL/templates-tsql/Get-SQLOSFileOpenRowSet.sql
+++ Stable-ish/MSSQL/templates-tsql/Get-SQLOSFileOpenRowSet.sql
@@ -1,3 +1,4 @@
 -- select the contents of a file using openrowset
 -- note: ad-hoc queries have to be enabled
+-- https://docs.microsoft.com/en-us/sql/t-sql/functions/openrowset-transact-sql
 SELECT cast(BulkColumn as varchar(max)) as Document FROM OPENROWSET(BULK N'C:\windows\temp\blah.txt', SINGLE_BLOB) AS Document
