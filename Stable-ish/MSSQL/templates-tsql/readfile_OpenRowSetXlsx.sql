2f17053aca7008ca3492d09825363e84b08b5775
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-06 08:55:46 -0500

    Update readfile_OpenRowSetXlsx.sql

--- Stable-ish/MSSQL/templates-tsql/readfile_OpenRowSetXlsx.sql
+++ Stable-ish/MSSQL/templates-tsql/readfile_OpenRowSetXlsx.sql
@@ -1,4 +1,16 @@
 
+-- Requires the driver be installed ahead of time.
+
+-- Enable show advanced options
+sp_configure 'show advanced options',1
+reconfigure
+go
+
+-- Enable ad hoc queries
+sp_configure 'ad hoc distributed queries',1
+reconfigure
+go
+
 -- Read text file from disk
 SELECT column1 FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0', 'Excel 12.0;Database=C:\windows\temp\Book1.xlsx;', 'SELECT * FROM [Targets$]')
 
