16c38bf518be1b9d8c4da23f8be393cc74ded009
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-06 08:35:57 -0500

    Update readfile_OpenRowSet2.sql

--- Stable-ish/MSSQL/templates-tsql/readfile_OpenRowSet2.sql
+++ Stable-ish/MSSQL/templates-tsql/readfile_OpenRowSet2.sql
@@ -1,5 +1,5 @@
 -- Note: Requires the driver to be installed ahead of time.
 -- EXEC sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1  -- not required
 -- EXEC sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1  -- not required
-EXEC master..xp_regwrite 'HKEY_LOCAL_MACHINE','SOFTWARE\Microsoft\Jet\4.0\Engines','SandBoxMode','REG_DWORD',1;
+-- EXEC master..xp_regwrite 'HKEY_LOCAL_MACHINE','SOFTWARE\Microsoft\Jet\4.0\Engines','SandBoxMode','REG_DWORD',1; -- not required
 SELECT * FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0','Text;Database=c:\temp\;HDR=Yes;FORMAT=text', 'SELECT * FROM [file.txt]')
