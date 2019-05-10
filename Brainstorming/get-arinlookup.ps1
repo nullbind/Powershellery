8bd750151285b4bb19c22d085f83b37db98db8a7
Scott Sutherland <nullbind@users.noreply.github.com>
2019-05-10 10:50:09 -0500

    Create get-arinlookup.ps1

---
+++ Brainstorming/get-arinlookup.ps1
@@ -0,0 +1,54 @@
+ # Get list of networks
+ $MyIPs = gc C:\temp\networks.txt
+     
+ # IP info table
+$TblIPInfo = new-object System.Data.DataTable
+$TblIPInfo.Columns.Add("IpDest") | Out-Null
+$TblIPInfo.Columns.Add("IpSrc") | Out-Null
+$TblIPInfo.Columns.Add("Owner") | Out-Null
+$TblIPInfo.Columns.Add("StartRange") | Out-Null
+$TblIPInfo.Columns.Add("EndRange") | Out-Null
+$TblIPInfo.Columns.Add("Country") | Out-Null
+$TblIPInfo.Columns.Add("City") | Out-Null
+$TblIPInfo.Columns.Add("Zip") | Out-Null
+$TblIPInfo.Columns.Add("ISP") | Out-Null 
+
+# Lookup source IP owner 
+$MyIPs | ForEach-Object {
+
+    # Get source IP
+    $IpAddress = $_ -split(" ")    
+
+    # Send whois request to arin via restful api
+    $targetip = $IpAddress[0]
+    Write-Output "Looking up $targetip..."
+    $web = new-object system.net.webclient
+    [xml]$results = $web.DownloadString("http://whois.arin.net/rest/ip/$targetip")
+
+    # Parse data from responses    
+    $IpOwner = $results.net.name 
+    $IpStart = $results.net.startAddress
+    $IpEnd = $results.net.endaddress  
+    $IpCountry = $results2.query.country.'#cdata-section'
+    $IpCity = $results2.query.city.'#cdata-section'
+    $IpZip = $results2.query.zip.'#cdata-section'
+    $IpISP = $results2.query.isp.'#cdata-section'
+
+    # Put results in the data table   
+    $TblIPInfo.Rows.Add("$CurrentDest",
+      "$IpAddress",
+      "$IpOwner",
+      "$IpStart",
+      "$IpEnd",
+      "$IpCountry",
+      "$IpCity",
+      "$IpZip",
+      "$IpISP") | Out-Null
+
+    # status the user
+    Write-Verbose "Dest:$CurrentDest Src:$IpAddress Owner: $IpOwner ($IpCountry) ($IpStart -$IpEnd)"
+    
+}
+
+# Display results
+$TblIPInfo | Out-GridView
